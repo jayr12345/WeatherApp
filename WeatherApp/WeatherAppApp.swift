c54dc6ef361a4af6e46c747dea9aa1aea7dff95f
JayR <jayratamosaatamosajr@JayRs-Mac-mini.local>
2023-03-11 08:38:19 +0800

    Initial Code base for the Weather application

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by JayR Atamosa on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
